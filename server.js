402c03d088401d49a699539bca46ded743c3a780
kaysush <kumar.sushil@outlook.com>
2014-04-27 23:46:06 +0530

    Google Authentication

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google').Strategy;
+var express = require('express');
+var app = express();
+var port = Number(process.env.PORT || 8080);
+
+passport.use( new GoogleStrategy({
+	returnURL: 'http://hdccommunicator.herokuapp.com/auth/google/return',
+	realm: 'http://hdccommunicator.herokuapp.com'
+}, function(identifier, profile, done){
+	User.findOrCreate({openId: identifier}, function(err,user){
+		done(err,user)
+	});
+} 
+));
+app.get('/' , function(req, res){
+	res.send('Hello World...');
+} )
+app.get('/auth/google', passport.authenticate('google'));
+app.get('/auth/google/return', 
+  passport.authenticate('google', { successRedirect: '/',
+                                    failureRedirect: '/login' }));
+
+app.listen(port,function(){
+	console.log('Server Started at ' + port);
+});

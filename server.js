aab134bf8bf98f52e7fac46ce5bac3f0514a9b81
kaysush <kumar.sushil@outlook.com>
2014-05-03 12:25:06 +0530

    Server re-written

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-var passport = require('passport');
-var GoogleStrategy = require('passport-google').Strategy;
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var socketio = require('socket.io');
+var io = socketio.listen(server);
 var port = Number(process.env.OPENSHIFT_NODEJS_PORT || 8080);
 var ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-passport.use( new GoogleStrategy({
-	returnURL: 'http://communicator-sushilkumar.rhcloud.com/auth/google/return',
-	realm: 'http://communicator-sushilkumar.rhcloud.com'
-}, function(identifier, profile, done){
-	User.findOrCreate({openId: identifier}, function(err,user){
-		done(err,user)
-	});
-} 
-));
-app.get('/' , function(req, res){
-	res.send('Hello World...');
-} )
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/return', 
-  passport.authenticate('google', { successRedirect: '/',
-                                    failureRedirect: '/login' }));
 
-app.listen(port,ipAddress,function(){
-	console.log('Server Started at ' + port);
+app.get('/',function(request, response){
+	response.send('Hello World');
 });
+
+
+io.sockets.on('connection', function(socket){
+
+	socket.on('message' , function(message){
+		console.log(data);
+		socket.broadcast.emit('message' , message);
+	})
+
+	socket.on('disconnect' , function(){
+		console.log('A client left');
+	})
+
+});
+
+
+server.listen(port , ipAddress, 	function(){
+	console.log('Server started at %s:%d',ipAddress,port);
+});
+
